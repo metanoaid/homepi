51c4cbe56b112210b04cb02a14e0ba30fbb76916
Victor Glembitskiy <metanoaid@gmail.com>
2021-03-13 20:28:56 +0300

    switched to map

--- autostart.sh
+++ autostart.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#chromium-browser \
-#    --no-first-run \
-#    --disable \
-#    --disable-translate \
-#    --disable-infobars \
-#    --disable-suggestions-service \
-#    --disable-save-password-bubble \
-#    --start-maximized \
-#    --kiosk \
-#    -app="http://192.168.0.21/index.html" &
+chromium-browser \
+    --no-first-run \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-save-password-bubble \
+    --start-maximized \
+    --kiosk \
+    -app="http://192.168.0.21/index.html" &
 
 unclutter -idle 0.5 -root &
 
-/home/pi/homepi/camera.sh&
+#/home/pi/homepi/camera.sh&
 
 xset s noblank
 xset s off
