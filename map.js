a964a80f4e2e88bffb956361b95ec641394f34eb
Victor Glembitskiy <metanoaid@gmail.com>
2021-03-13 21:46:21 +0300

    changed map.js and added reload.sh

--- map.js
+++ map.js
@@ -1,13 +1,38 @@
-var myMap;
+//var myMap;
+//ymaps.ready(init);
+
+//function init () {
+//    myMap = new ymaps.Map('map', {
+//        center: [59.85, 30.31], 
+//        zoom: 13,
+//        controls: []
+//    });
+//control = myMap.controls.get('trafficControl');
+//control.showTraffic();
+//var trafficControl = new ymaps.control.TrafficControl({state: {trafficShown: true}});
+//map.controls.add(trafficControl, {top: 10, left: 10});
+//var actualProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
+//actualProvider.setMap(myMap);
+//}
+
 ymaps.ready(init);
 
 function init () {
-    myMap = new ymaps.Map('map', {
-        center: [59.852011, 30.310520], 
-        zoom: 13,
-        controls: []
-    });	
+    var myMap = new ymaps.Map('map', {
+            center: [59.85, 30.31],
+            zoom: 13,
+            controls: []
+        });
 
-var actualProvider = new ymaps.traffic.provider.Actual({}, { infoLayerShown: true });
-actualProvider.setMap(myMap);
-}
+    // Создадим элемент управления "Пробки".
+    var trafficControl = new ymaps.control.TrafficControl({ state: {
+            // Отображаются пробки "Сейчас".
+            providerKey: 'traffic#actual',
+            // Начинаем сразу показывать пробки на карте.
+            trafficShown: true
+        }});
+    // Добавим контрол на карту.
+    myMap.controls.add(trafficControl);
+    // Получим ссылку на провайдер пробок "Сейчас" и включим показ инфоточек.
+    trafficControl.getProvider('traffic#actual').state.set('infoLayerShown', true);
+}k
