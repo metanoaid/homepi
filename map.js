2f14e39fb6e219dfff8d0fd216c3b29d5b839923
Victor Glembitskiy <metanoaid@gmail.com>
2021-03-13 20:32:06 +0300

    changed gps in map.js

--- map.js
+++ map.js
@@ -3,7 +3,7 @@ ymaps.ready(init);
 
 function init () {
     myMap = new ymaps.Map('map', {
-        center: [59.85571984, 30.34642999], 
+        center: [59.852011, 30.310520], 
         zoom: 13,
         controls: []
     });	
