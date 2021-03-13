b42ecc51192d5e2148385d4a8c76d953b8772359
Victor Glembitskiy <metanoaid@gmail.com>
2021-03-13 18:54:40 +0300

    added autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,16 @@
+chromium-browser \
+    --no-first-run \
+    --disable \
+    --disable-translate \
+    --disable-infobars \
+    --disable-suggestions-service \
+    --disable-save-password-bubble \
+    --start-maximized \
+    --kiosk \
+    -app="http://192.168.0.21/index.html" &
+
+#/home/pi/Video.sh&
+
+xset s noblank
+xset s off
+xset s -dpms
