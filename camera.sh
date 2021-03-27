a593aaf628e6e35e4a32427e07c192ef82f58c00
Victor Glembitskiy <metanoaid@gmail.com>
2021-03-27 23:02:16 +0300

    camera.sh less logs

--- camera.sh
+++ camera.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while  true ; do
-	cvlc -vvv rtsp://admin:22sS8XQtKv@192.168.0.20:554/Streaming/Channels/2 --width=640 --height=480 --video-x=300 --video-y=300 --no-audio --no-video-title-show
+	cvlc -v rtsp://admin:22sS8XQtKv@192.168.0.20:554/Streaming/Channels/2 --width=640 --height=480 --video-x=300 --video-y=300 --no-audio --no-video-title-show
 	sleep 5
 done
